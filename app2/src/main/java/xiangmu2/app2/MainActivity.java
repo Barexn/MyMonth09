076a165e4f6ea3ed35491f4864e42736c0f2bd8f
Barexn <2687698398@qq.com>
2017-09-27 13:56:10 +0800

    2222222

--- app2/src/main/java/xiangmu2/app2/MainActivity.java
+++ app2/src/main/java/xiangmu2/app2/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //第一次修改
+
+        //再次修改
     }
 }
